c8648af24210ab2d8cea7130ab74538492c1319a
Rafael Corsi <corsiferrao@gmail.com>
2023-05-01 11:00:42 -0500

    removendo solucao

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -1,16 +1,16 @@
 #include "stdio.h"
 // ..
 
-int foo(int a) { return a + 1; }
+int a = 0;
+int b = 0;
+
+int foo(void) { return a + 1; }
 
 void main(void) {
   // ..
 
-  int a = 0;
-  int b = 0;
-
   while (1) {
-    a = foo(a);
+    foo();
 
     if (a > 5) {
       b = 1;
