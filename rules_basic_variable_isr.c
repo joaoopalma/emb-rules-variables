2322e2e690c94482d6b3db282b32a80841453da6
Rafael Corsi <corsiferrao@gmail.com>
2023-05-08 16:34:06 -0500

    update basic variable isr

--- rules_basic_variable_isr.c
+++ rules_basic_variable_isr.c
@@ -1,28 +1,21 @@
 #include "asf.h"
 
 int g_cnt = 0;
-char g_str[10];
+int f_btn = 0; // deveria ser volatile
 
-// This code creates a progress bar on an OLED screen that
-// increases when the button is pressed.
+// ISR
 void btn_callback(void) {
-  printf("btn pressed \n");
-
-  if (g_cnt >= 8)
-    g_cnt = 0;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  f_btn = 1; // variável alterada de uma interrupcão
 }
 
 void main(void) {
-  // ...
+
+  volatile int cnt = 0; // não precisa ser volatile
 
   while (1) {
+    if (f_btn) {
+      cnt++;
+      f_btn = 0;
+    }
   }
 }
