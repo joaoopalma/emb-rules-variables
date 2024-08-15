9a9a2a3f00fc9a125cc48c296410e03091a39648
joaoopalma <103431544+joaoopalma@users.noreply.github.com>
2024-08-15 11:25:32 -0300

    Update rules_basic_variable.c

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -1,20 +1,16 @@
 #include "stdio.h"
 
-// ..
-
-int a = 0; // var global
-
-int b = 0; // var global
-
-
-void foo(void) {
-    a = a + 1; // acessa variavel global
+void foo(int &a) {
+    *a = *a + 1; // acessa ponteiro
 }
 
 void main(void) {
+    int a = 0;
+    int b = 0;
+
     while (1) {
-        foo();
-        
+        a = foo(&a);
+
         if (a > 5) {
             b = 1;
         }
